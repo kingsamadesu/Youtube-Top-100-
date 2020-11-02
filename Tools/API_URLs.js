3ca91dc6d764fbd79336d2cac02b3f4fd6521d1a
Ismail Ou Khalek <ismailhivi@gmail.com>
2020-11-02 16:35:12 +0100

    change the Google API KEY

--- Tools/API_URLs.js
+++ Tools/API_URLs.js
@@ -1,4 +1,5 @@
-const API_Key = "AIzaSyBp-sUUbNlI-aCounGzGbvzvlybf_SboAg"
+const API_Key1 = "AIzaSyBp-sUUbNlI-aCounGzGbvzvlybf_SboAg"
+const API_Key = "AIzaSyChAEQXtgkbBUEzCYNcdEcjULQnfSV9hhM"
 
 function get_Url_channels(channelId){
     return 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=' + channelId + '&key=' + API_Key
@@ -15,7 +16,7 @@ function get_Url_commentThreads(videoId,pageToken="") {
     if (pageToken == ""){
         return "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=" + videoId + "&maxResults=100&key=" + API_Key;
     }
-    return "https://www.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&videoId=" + videoId + '&pageToken='+ pageToken + "&maxResults=100&key=" + API_Key;
+    return "https://www.googleapis.com/youtube/v3/commentThreads?part=  &videoId=" + videoId + "&pageToken=" + pageToken + "&maxResults=100&key=" + API_Key;
 }
 
 module.exports = {get_Url_channels,get_Url_PlaylistItems,get_Url_commentThreads}
